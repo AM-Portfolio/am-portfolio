2d3a2f2499114c7f202973fc4542c1700ba86ee2
Munish Kumar <40138449+ssd2658@users.noreply.github.com>
2025-03-04 03:10:35 +0530

    Records are getting inserted

--- portfolio-service/src/main/java/com/portfolio/mapper/PortfolioMapper.java
+++ portfolio-service/src/main/java/com/portfolio/mapper/PortfolioMapper.java
@@ -9,6 +9,7 @@ import com.am.common.amcommondata.model.PortfolioModel;
 import com.am.common.amcommondata.model.asset.AssetModel;
 import com.am.common.amcommondata.model.asset.equity.EquityModel;
 import com.am.common.amcommondata.model.asset.mutualfund.MutualFundModel;
+import com.am.common.amcommondata.model.enums.AssetType;
 import com.am.common.amcommondata.model.enums.FundType;
 import com.portfolio.kafka.model.PortfolioUpdateEvent;
 
@@ -18,11 +19,13 @@ public class PortfolioMapper {
     public PortfolioModel toPortfolioModel(PortfolioUpdateEvent portfolioEvent) {
         PortfolioModel portfolioModel = PortfolioModel.builder()
         .id(portfolioEvent.getId())
+        .name("Default Portfolio")
         .owner(portfolioEvent.getUserId())
         .fundType(FundType.DEFAULT)
         .status("Active")
         .createdBy(portfolioEvent.getUserId())
         .assets(mapToAssets(portfolioEvent))
+        .version(0L)
         .build();
         return portfolioModel;
     }
@@ -35,17 +38,45 @@ public class PortfolioMapper {
         Set<AssetModel> assets = new HashSet<>();
         
         if (equities != null) {
-            assets.addAll(equities.stream()
-                .map(equity -> (AssetModel) equity)
-                .collect(Collectors.toSet()));
+            var assetModels = equities.stream()
+            .filter(e -> e.getIsin() != null && e.getSymbol() != null)
+            .map(this::mapEquityModelToAsset)
+            .collect(Collectors.toSet());
+            assets.addAll(assetModels);
         }
         
         if (mutualFunds != null) {
-            assets.addAll(mutualFunds.stream()
-                .map(fund -> (AssetModel) fund)
-                .collect(Collectors.toSet()));
+            var fundModels = mutualFunds.stream()
+                .filter(e -> e.getIsin() != null && e.getSymbol() != null)
+                .map(this::mapToAsset)
+                .collect(Collectors.toSet());
+            assets.addAll(fundModels);
         }
-        
+
         return assets;
+    }
+
+    private AssetModel mapEquityModelToAsset(EquityModel equityModel) {
+        return AssetModel.builder()
+        .assetType(AssetType.EQUITY)
+        .isin(equityModel.getIsin())
+        .symbol(equityModel.getSymbol())
+        .name(equityModel.getName())
+        .avgBuyingPrice(equityModel.getAvgBuyingPrice())
+        .name(equityModel.getName())
+        .quantity(equityModel.getQuantity())
+        .build();
+    }
+
+    private AssetModel mapToAsset(MutualFundModel fundModel) {
+        return AssetModel.builder()
+        .assetType(AssetType.MUTUAL_FUND)
+        .isin(fundModel.getIsin())
+        .symbol(fundModel.getSymbol())
+        .name(fundModel.getName())
+        .avgBuyingPrice(fundModel.getAvgBuyingPrice())
+        .name(fundModel.getName())
+        .quantity(fundModel.getQuantity())
+        .build();
     }
 }
